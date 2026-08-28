c3b88847425906ef27be2f3feeffb7d785a67c1f
Marco Donadio <mfdonadio@correo.url.edu.gt>
2026-08-28 14:10:05 -0600

    fix: errores. feature: maso la mitad del codigo jeje

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ class AFD:
             print("-" * len(encabezado))
 
             #Vamos llenando las filas de la tabla con los estados y sus transiciones
-            for estado in estados_ordenado:
+                for estado in estados_ordenados:
                 fila = estado.ljust(ancho)
 
                 for simbolo in alfabeto_ordenado:
@@ -171,6 +171,172 @@ class ValidadorAFD:
                         + simbolo
                         + ")."
                     )
-
+        #Booleano que nos indica si, en efecto, el AFD es valido o no.
+        #Si la lista de errores esta vacia, es valido, si no, pues no.
         afd.es_valido = len(errores) == 0
+        #Retornamos el booleano y la lista de errores
         return afd.es_valido, errores
+
+    @staticmethod
+    def analizar_estructura(afd):
+        """
+        Esta funcion analiza las estrcutura del AFD para calcular:
+            1. Los estados alcanzables
+            2. Los estados inalcanzables
+            3. Los estados inaccesibles
+            4. Los estados finales alcanzables
+            5. Por ultimo, la posibilidad de lenguaje vacio (que el afd acepte cadenas vacias, epselon).
+        """
+
+        #Creamos un set para almacenar los estados alcanzables
+        estados_alcanzables = set()
+        #Inicializamos una variable "pendientes" con el estado inicial del AFD
+        pendientes = [afd.estado_inicial]
+
+        #Para recorrer los estados pendientes, utilizamos un recorrido tipo DFS haciendo uso de una lista commo pila.
+        while len(pendientes) > 0:
+            estado_actual = pendientes.pop()
+
+            if estado_actual in estados_alcanzables: continue
+
+            #Agregamos el estado actual a los estados alcanzables
+            estados_alcanzables.add(estado_actual)
+
+            #Ahora hacemos un for para recorrer el alfabeto y buscar los estados alcanzables desde el estado actual
+            for simbolo in afd.alfabeto:
+                #Para obtener el estado siguiente, hacemos uso del diccionario de transiciones del AFD, utilizando la tupla (estado_actual, simbolo) como clave.
+                siguiente = afd.transiciones.get((estado_actual, simbolo))
+
+                #Si el estado siguiente no es None (null) y el siguiente no esta en los estados inalcanzables...
+                #agregamos el siguiente estado a la lista de pendientes
+                if siguiente is not None and siguiente not in estados_alcanzables:
+                    pendientes.append(siguiente)
+
+        inaccesibles = afd.estados - estados_alcanzables
+        finales_alcanzables = afd.estados_finales.intersection(estados_alcanzables)
+        lenguaje_posiblemente_vacio = len(finales_alcanzables) == 0
+
+        return{
+            "alcanzables": estados_alcanzables,
+            "inaccesibles": inaccesibles,
+            "finales_alcanzables": finales_alcanzables,
+            "lenguaje_posiblemente_vacio": lenguaje_posiblemente_vacio,
+        }
+    
+class SimuladorAFD:
+    """
+    Evalua el automata (generacion completa de la traza y del comportamiento)
+    """
+
+    @staticmethod
+    def evaluar(afd, cadena, mostrar_traza=True):
+        if not afd.es_valido:
+            return False, "El AFD debe validarse antes de evaluar cadenas."
+
+        #De primero, comprobamos que todos los simbolos pertenezcan al alfabeto.
+        for simbolo in cadena:
+            if simbolo not in afd.alfabeto:
+                return False,(
+                    "La cadena contiene el simbolo "
+                    + simbolo
+                    + " , que no pertenece al alfabeto."
+                )
+
+            #Ahora definimos el estado inicial
+            estado_actual = afd.estado_inicial
+            #Y comenzamos con la traza... :D
+            if mostrar_traza:
+                print("\n--- TRAZA DE EJECUCION ---")
+                print("Estado inicial: ", estado_actual)
+
+                #Por cada simbolo... mostramos su traza
+                for simbolo in cadena:
+                    siguiente_estado = afd.transiciones[(estado_actual, simbolo)]
+
+                    if mostrar_traza:
+                        print(
+                            estado_actual
+                            + " ---"
+                            + simbolo 
+                            + " -->"
+                            + siguiente_estado
+                        )
+
+                    #Nos movemos de estado
+                    estado_actual = siguiente_estado
+
+                #Evaluamos si el estado esta entre el conjunto de estados finales
+                aceptada = estado_actual in afd.estados_finales
+                resultado = "Aceptada" if aceptada else "Rechazada"
+
+                #Mostramos el resumen de la evaluacion
+                if mostrar_traza:
+                    print("Estado final: ", estado_actual)
+                    print("Resultado: " + resultado)
+
+                #Actualizamos el historial
+                afd.historial.append(
+                    {
+                        "cadena": cadena, 
+                        "estado_final": estado_actual,
+                        "resultado": resultado,
+                    }
+                )
+                #Retornamos su estado de aceptacion
+                return aceptada, resultado
+
+class CargadorAFD:
+    """ Clase creada con el din de responsabiliarse de crear los AAFD manualmente o dese la carga 
+    de un archivo externo.
+    """
+
+    @staticmethod
+    def pedir_conjunto(mensaje, permitir_vacio=False):
+        """
+        Este metodo solicita elementos separados por coma y evalua duplicados.
+        Devuelve un set unicamente cuando la entrada es valida.
+        """
+
+        while True:
+            #Obtenemos la entrada
+            entrada = input(mensaje).strip()
+
+            #Si la entrada no contiene texto (acepta el vacio)
+            if entrada == "" and permitir_vacio:
+                return set()
+
+            #Si la entrada no acepta el vacio
+            if entrada == "":
+                print("La entrada no puede estar vacia.")
+                continue
+
+            #Obtenemos la partes del AFD
+            partes = entrada.split(",")
+            #Inicializamos la lista de elementos
+            elementos = []
+            #Inicializamos la variable de hay_vacios como "False"
+            hay_vacios = False
+
+            #Para cada elemento contenido en partes...
+            for parte in partes:
+                elemento = parte.strip()
+
+                #Si el elemento esta vacio
+                if elemento == "":
+                    hay_vacios = True
+                    break
+
+                #Añadimos los elementos validos a la lista
+                elementos.append(elemento)
+
+            #Si existieron vacios enla entrada
+            if hay_vacios:
+                print("No se permiten elementos vacios entre comas.")
+                continue
+
+            #Si la longitud de los elementos difiere 
+            if len(elementos) != len(set(elementos)):
+                print("No se permiten estados o símbolos duplicados.")
+                continue
+
+            return set(elementos)
